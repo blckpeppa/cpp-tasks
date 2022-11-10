f3a7e0a923d9c4bd6023bd40c7808db29dd3988e
gambi <35289169+blckpeppa@users.noreply.github.com>
2022-11-10 20:13:40 +0300

    initial commit

---
+++ explicit example.cpp
@@ -0,0 +1,15 @@
+// C++ program to demonstrate explicit type conversion
+
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+    
+    float myNum=10.123;
+    int myNewNum=int(myNum);
+    cout << myNewNum;
+	return 0;
+}
+
